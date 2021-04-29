fba9558b78838bf52241a32b831810a6651c2d6d
Matias Lopez <mati021796@gmail.com>
2021-04-29 13:19:21 -0300

    finalpush

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ app.use((req, res, next) => {
     next();
 });
 
-var whitelist = ['http://birellogallery.com', 'https://birellogallery.com', 'http://www.birellogallery.com', 'https://www.birellogallery.com', 'http://localhost:4200', 'http://localhost:4000', 'http://localhost:3000' ];
+var whitelist = ['http://birellogallery.com', 'https://birellogallery.com', 'http://www.birellogallery.com', 'https://www.birellogallery.com' ];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('Not allowed by CORS'));
     }
   }
 }
