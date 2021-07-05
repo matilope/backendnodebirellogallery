19bad4113ade2e8f8025ffdc57018eef85ee4a82
matilope <66441937+matilope@users.noreply.github.com>
2021-07-05 19:57:02 -0300

    Update contact.js

--- routes/contact.js
+++ routes/contact.js
@@ -22,15 +22,15 @@ router.post("/formulario", accountLimiter, (req, res) => {
       port: 465,
       secure: true,
       auth: {
-        user: 'birelloignacio@gmail.com',
-        pass: 'mpuxmaqsbrfulfur'
+        user: '********@******.com',
+        pass: '******'
       }
     });
 
     if (!validator.isEmpty(req.body.name && req.body.email && req.body.subject && req.body.textarea && req.body.paint)) {
       const mailOptions = {
-        from: `"Birello Gallery" <birelloignacio@gmail.com>`,
-        to: `"ignaciobirello@hotmail.com"`,
+        from: `"Birello Gallery" <********@*******.com>`,
+        to: `"**********@*******.com"`,
         subject: `${req.body.subject}`,
         html: `<ul style="line-height:30px;"><li><strong>Name: </strong>${req.body.name}</li><li><strong>Subject: </strong>${req.body.subject}</li><li><strong>Email: </strong>${req.body.email}</li><li><strong>Option: </strong>${req.body.paint}</li><li><strong>Message: </strong>${req.body.textarea}</li></ul>`
       };
